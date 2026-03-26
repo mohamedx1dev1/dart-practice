8c4a550fe9a1ac9efe6d476657f6824b9d3b0d10
mohamedx1dev1 <mohamedx1dev1@gmail.com>
2026-03-26 21:07:17 +0200

    Delete loop(for).dart

--- loop(for).dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print("enter number");
-  int num = int.parse(stdin.readLineSync()!);
-  for (int i = 1; i <= num; i++) {
-    print(" $i*$num=${i*num}");
-  }
-}
